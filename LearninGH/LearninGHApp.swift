ba09d6a632465a54805accef1f0ec62bd1f7ae5d
Maria Tkachenkova <mashynja1795@gmail.com>
2023-01-05 18:25:54 +0400

    Initial Commit

---
+++ LearninGH/LearninGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearninGHApp.swift
+//  LearninGH
+//
+//  Created by Maria Tkachenkova on 05.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct LearninGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
